9131729492009ee6202c2afe347bf1689bd66bed
Darren-Mc <darren.mcmorran@gmail.com>
2015-08-07 11:32:16 +1000

    Final Version
    Hopefully..

--- rotaryencoderanalog/rotaryencoderanalog.m
+++ rotaryencoderanalog/rotaryencoderanalog.m
@@ -1,93 +1,66 @@
-clear;
-clc;
+% MEC2407 ELECTROMECHANICS - Computer Lab Part 3 Solution %
+% Written by Darren McMorran %
+clear; clc;
 data = importdata('data.txt','\t');
-res = 200;
-fn = 10;
-fl = 200;
+res = 200; % The rotary encoder resolution in pulses / revolution
 n = 1:length(data);
-st = 360e-6; % Sample Period
+st = 360e-6; % Sample Period (determined by Arduino)
 sf = 1/st; % Sample Frequency
 
-A = data(:,1)' > 125;
-B = data(:,2)' > 125;
-Z = data(:,3)' > 125;
-% time = data(:,4)'/1e6;
-t = linspace(0,st*length(data),length(data));
-% 
-dA = diff(A)==1;
-At = t(dA);
-dAt = diff(At);
-% As = 60./(res*At);
-dAB = dA.*(2*(B(1:numel(dA))-1/2));
-dAB2 = dAB(dAB~=0);
-t2 = t(dAB~=0);
-dABsum = cumsum(dAB2);
-pos = dABsum./res;
-pos2 = cumsum(dAB)./res;
-dpos = diff(pos);
-dt2 = diff(t2);
-speed = 60*dpos./dt2;
-speedf = filter(1/fn*ones(fn,1),1,speed);
+A = data(:,1)' > 125; % Convert signal A to digital
+B = data(:,2)' > 125; % Convert signal B to digital
+C = data(:,3)'; % Absolute angular position
 
+t = linspace(0,st*(length(data)-1),length(data)); % Time vector
+
+dA = diff(A)==1; % Signal A Rising Pulses
+t2 = t(2:end); % Time vector corresponding to dA vector
+At = t2(dA); % Time vector of signal A rising pulses
+dAt = diff(At); % Time difference between signal A rising pulses
+
+% Determine Angular Position
+dAB = dA.*(-2*(B(1:numel(dA))-1/2)); % Scale with direction
+% If A leads B: at a rising pulse in A, signal B is 0 so dAB = +1
+% If B leads A: at a rising pulse in A, signal B is 1 so dAB = -1
+dpos = dAB./res; % Scale to correct angular position (revolutions) 
+pos = cumsum(dpos); % Position = integral of change in position (dpos)
+
+% Determine Angular Velocity
+dpos = dpos(dA); % Take non-zero values
+speed = 60*dpos(2:end)./dAt; % Calculate angular velocity (rpm)
+
+% Filter 1 (simple)
+f1 = 10; % Filter 1 length
+speedf1 = filter(1/f1*ones(f1,1),1,speed); % Apply mean filter
+
+% Filter 2 removes stationary errors that result from the mean filter
+% Extend speed vector to size of input data at correct time scale
 j = 1;
 speedj = 0;
-speed2 = zeros(size(t));
+speed2 = zeros(size(t)); % Initialise vector to improve efficiency
 for i = 1:numel(t)
-    if (t2(j) == t(i))
-        speedj = speed(j);
+    if (At(j) == t(i)) % If there is a new speed calculated at this time
+        speedj = speed(j); % Store most recently calculated speed
         if (j+1 <= numel(speed) )
             j = j+1;
         end
     end
-    speed2(i) = speedj;
+    speed2(i) = speedj; % Extend speed vector
 end
 
-fn = 200; 
-speedf2 = filter(1/fn*ones(fn,1),1,speed2);
+f2 = 200; % Filter 2 length
+speedf2 = filter(1/f2*ones(f2,1),1,speed2); % Apply mean filter 2
 
-% dpost = pos(dpos~=0);
-% test = 6000./(res*diff(dABsum));
-%dABt = Bt(1:numel(dAt)).*dAt;
-% dAt2 = 60./(res*dAt);
-% speed = 0;
-% j=0;
-% s = zeros(size(dAB));
-% for i=1:numel(dAB)
-%     if ( dA(i) == 1 )
-%         if( j > 0 )
-%             speed = dAt2(j);
-%         end
-%         j=j+1;
-%     elseif ( dAB(i) == -1 )
-%         speed = -dAt2(j);
-%         j=j+1;
-%     end
-%     s(i) = speed;
-% end
-% 
-% for i=(fl+1):numel(dAB)
-%     s(i)=(dABsum(i)-dABsum(i-fl))/(t(i)-t(i-fl));
-% end
-
-        
-% fdAt = filter(1/fn*ones(fn,1),1,dAt2);
-% % 
-% dZ = diff(Z)==1;
-% Zt = time(diff(Z)==1);
-% dZt = 60./diff(Zt);
-% 
-% nt = n(diff(Z)==1);
-% nt2 = n(diff(Z)==-1);
-% nt2-nt;
-% 
-% for i=1:numel(nt)-1
-% sum(dA(nt(i):nt(i+1)))
-% end
-%
-plot(t(2:end),10*pos2,'k',t2(2:end),speed,'b',t2(2:end),speedf,'g',t,speedf2,'r');
-%plot(linspace(0,st*length(data),numel(pos)),pos);%,linspace(0,st*length(data),numel(s)),s);
-%plot(linspace(0,st*length(data),numel(dAt2)),dAt2,linspace(0,st*length(data),numel(fdAt)),fdAt,'r');%,Bt(1:numel(fdBt)),fdBt,Zt(1:numel(dZt)),dZt);
-xlabel('Time (s)');
-ylabel('Angular Velocity (RPM)');
-legend('Angular Position','Raw','Mean Filtered (n=10)','Mean Filtered (n=100)');
-%plot(Z)
+% Display two plots simultaneously showing angular position and velocity
+subplot(2,1,1);
+plot(t2,pos,'r',t,C,'b');
+title('Rotary Encoder Angular Position','fontweight','bold');
+xlabel('Time (s)','fontweight','bold');
+ylabel('Angular Position (revolutions)','fontweight','bold');
+legend('Estimated Position','Given Position');
+subplot(2,1,2);
+plot(At(2:end),speed,'b',At(2:end),speedf1,'g',t,speedf2,'r');
+title('Rotary Encoder Angular Velocity','fontweight','bold');
+xlabel('Time (s)','fontweight','bold');
+ylabel('Angular Velocity (RPM)','fontweight','bold');
+legend('Estimated Velocity','Filtered Velocity 1','Filtered Velocity 2');
