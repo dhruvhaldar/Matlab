7fb9d07cb107535a81bedbd6d091d2432fec84f0
Darren-Mc <darren.mcmorran@gmail.com>
2015-08-07 16:29:53 +1000

    Find Limit Working As Intended
    Determines function based on movement vector

--- AL5D Simulation/findLimit.m
+++ AL5D Simulation/findLimit.m
@@ -1,5 +1,12 @@
-function [er,et,t] = findLimit(ri, r, delta)
-    fun = @(r,C) (1007958012753983*acos((10*((2*r - 1207/10)^2 + r^2 - 3782334602438243/274877906944))/(2921*((2*r - 1207/10)^2 + r^2)^(1/2))))/17592186044416 + (1007958012753983*angle(r*(1 - 2*i) + (1207*i)/10))/17592186044416-C;
+function [er,et,t] = findLimit(ri, hi, dr, dh, r, delta)
+    global BASE_HEIGHT HUMERUS ULNA;
+    h = @(r)(dh/dr)*(r-ri)+hi;
+    s2w_r = @(r)r;
+    s2w_h = @(r)h(r)-BASE_HEIGHT;
+    s2w = @(r)( s2w_h(r) * s2w_h(r) ) + ( s2w_r(r) * s2w_r(r) ); % (Length of line from shoulder to wrist (S-W))^2
+    a1 = @(r) atan2( s2w_h(r),s2w_r(r) ); % Angle between S-W line and ground
+    a2 = @(r) acos((( HUMERUS^2 - ULNA^2 ) + s2w(r) ) / ( 2 * HUMERUS * sqrt( s2w(r) ) )); % Angle between S-W line and humerus
+    fun = @(r,C) radtodeg(a1(r)+a2(r))-C;
     t = fun(r,0);
 
     ct=t+delta;
@@ -41,8 +48,8 @@ function [er,et,t] = findLimit(ri, r, delta)
 
     if isnan(cr)
         if (isnan(dr))
-            er = NaN;
-            et = NaN;
+            er = ri;
+            et = fun(ri,0);
         else
             er = dr;
             et = dt;
