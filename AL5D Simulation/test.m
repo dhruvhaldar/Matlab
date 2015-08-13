6a52546bcf32b12c0b75d051253fa82bf6fbcd3b
Darren-Mc <darren.mcmorran@gmail.com>
2015-08-13 16:42:10 +1000

    Added GIF Export Function

--- AL5D Simulation/test.m
+++ AL5D Simulation/test.m
@@ -1,9 +1,12 @@
 clear; clc;
 
+filename = 'testnew51.gif';
+
+
 global HAND;
 global threshold;
-threshold = 5;
-delta = 2;
+threshold = 10;
+delta = 8;
 dr = 0.5;
 dh = -1;
 hi = 315.05;
@@ -34,6 +37,10 @@ if (abs(r2(n)-rf(n)) < abs(r(n)-rf(n)))
     e(n) = e2(n);
 end
 pause(0.04);
+
+hFig = figure(1);
+set(hFig, 'Position', [100 100 1000 700])
+
 h = plot(rvec,thetavec,'b',rvec,evec,'k',rf(n),tf(n),'b*',rf(n),ef(n),'k*',[rvec(1) rvec(end)],[sp(n) sp(n)],'r:',[rvec(1) rvec(end)],[sm(n) sm(n)],'m:',[rvec(1) rvec(end)],[ep(n) ep(n)],'r:',[rvec(1) rvec(end)],[em(n) em(n)],'m:',r(n),s(n),'g*',r(n),e(n),'g*');
 % plot(r2,e2,'r*',r2,ep2,'g*',r2,em2,'b*');
 title('Postion Estimation Using Accelerometer Error','fontweight','bold')
@@ -50,4 +57,15 @@ set(gca, ...
   'YColor'      , [.3 .3 .3], ...
   'YTick'       , 0:50:350, ...
   'LineWidth'   , 1         );
+set(gcf,'color','w');
+
+      drawnow
+      frame = getframe(1);
+      im = frame2im(frame);
+      [imind,cm] = rgb2ind(im,256);
+      if n == 1;
+          imwrite(imind,cm,filename,'gif', 'Loopcount',inf,'DelayTime',0.04);
+      else
+          imwrite(imind,cm,filename,'gif','WriteMode','append','DelayTime',0.04);
+      end
 end
