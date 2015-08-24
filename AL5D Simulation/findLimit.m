b6a6fb1cf3818a6e7af1775e99f004a016f122e2
Darren-Mc <darren.mcmorran@gmail.com>
2015-08-25 09:23:31 +1000

    New Gif?

--- AL5D Simulation/findLimit.m
+++ AL5D Simulation/findLimit.m
@@ -33,4 +33,64 @@ function [ang1,ang2] = findLimit(fun1, fun2, ri, dr, R)
             ang2(i) = NaN;
         end
     end
+    
+    global threshold;
+    sp1= sfun(R,0)+threshold+phi;
+    rsp1 = findZeroPrev(@(r) sfun(r,sp1),ri,dr,R);
+    sm1 = sfun(R,0)+threshold-phi;
+    rsm1 = findZeroPrev(@(r) sfun(r,sm1),ri,dr,R);
+    sp2= sfun(R,0)-threshold+phi;
+    rsp2 = findZeroPrev(@(r) sfun(r,sp2),ri,dr,R);
+    sm2 = sfun(R,0)-threshold-phi;
+    rsm2 = findZeroPrev(@(r) sfun(r,sm2),ri,dr,R);
+    rsort1 = sort([R rsp1 rsp2 rsm1 rsm2]);
+    rvalues1 = rsort1(cumsum(rsort1==R)==0);
+    if (numel(rvalues1) == 1)
+        rlims = [ri rvalues1];
+    elseif (numel(rvalues1) > 1)
+        rlims = [rvalues1(end-1) rvalues1(end)];
+    else
+        rlims = [NaN NaN];
+    end
+
+    ep=efun(R,0)+threshold;
+    rp = findZeroPrev2(@(r) efun(r,ep),ri,rf,dr,R);
+    rp2 = findZeroNext2(@(r) efun(r,ep),ri,rf,dr,R);
+    rpo = rp;
+    rp2o = rp2;
+    if(abs(rp2o - rpo) < 0.01)
+        rp2o = rf;
+    end
+    if (isnan(rpo))
+        rpo = ri;
+    end
+    if (isnan(rp2o))
+        rp2o = rf;
+    end
+    em=efun(R,0)-threshold;
+    rm = findZeroPrev2(@(r) efun(r,em),ri,rf,dr,R);
+    rm2 = findZeroNext2(@(r) efun(r,em),ri,rf,dr,R);
+    rmo = rm;
+    if (isnan(rmo))
+        rmo = rp2o;
+    end
+    rm2o = rm2;
+    rsort2 = sort([R rp rm rp2 rm2]);
+    rvalues2 = rsort2(cumsum(rsort2==R)==0);
+    if (numel(rvalues2)>0)
+        rlim = rvalues2(end);
+    else
+        rlim = ri;
+    end
+    
+    if (rlim > rlims(1))
+        if (rlim < rlims(2))
+            rlims(1) = rlim;
+        else
+            rlims = [NaN NaN];
+        end
+    end
+
+    slims(1) = sfun(rlims(1),0);
+    slims(2) = sfun(rlims(2),0);
 end
